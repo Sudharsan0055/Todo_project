dc0bb01ca5294c510e15304e7d0f6892a2ecc6de
sudharsan0055 <babuiobapp@gmail.com>
2024-05-21 09:02:06 +0530

    commiting full code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,20 +7,11 @@ import { Todos } from './components/Todos'
 
 
 function App() {
-
-
   const [todos, setTodos] = useState([]);
-
-    fetch("http://localhost(3000)")
-    .then(async function(res) {
-      const json = await res.json();
-      setTodos(json.todos);
-    })
-
   return (
     <div>
       <CreateTodo />
-      <Todos todos={todos} />
+     <Todos />  
     </div>
   )
 }
--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,16 +1,47 @@
+import { useState } from "react";
 
 export function CreateTodo() {
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
     return <div>
         <input style = {{
             padding : 10,
              margin: 10}}
-         type="text" placeholder="title"></input> <br />
+         type="text" placeholder="title"
+         onChange={function(e) {
+            const value = e.target.value;
+            setTitle(e.target.value);
+         }}
+         ></input> <br />
         <input style = {{
             padding : 10,
-             margin: 10}}type="text" placeholder="description"></input> <br />
+             margin: 10}}type="text" placeholder="description"
+             onChange={function(e) {
+                const value = e.target.value;
+                setDescription(e.target.value);
+             }}
+             ></input> <br />
 
         <button style = {{
             padding : 10,
-             margin: 10}}>Add a todo</button>
+             margin: 10}}
+             onClick={ () => {
+                fetch("http://localhost:3000/todo", {
+                    method: "POST",
+                    body: JSON.stringify( {
+                        title: "",
+                        description: ""
+                    }),
+                    headers: {
+                        "content-type" : "application/json"
+                    }
+                })
+                .then(async (res) => {
+                    const json = await res.json();
+                    alert("Todo Added")
+                })
+             }} 
+             >Add a todo</button>
     </div>
 }
